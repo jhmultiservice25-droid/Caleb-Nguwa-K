0d625296f205d780f52d48a7e899490fcf4662c8
Caleb NGUWA K <jhmultiservice25@gmail.com>
2026-09-12 22:47:02 +0100

    feat(rdc): expose V1 country configuration modules

---
+++ countryconfig/src/index.ts
@@ -0,0 +1,10 @@
+export { RDC_COUNTRY_CONFIG } from './config/rdc'
+export { RDC_ROLES } from './config/roles'
+export { buildRegistrationNumber } from './config/registration-number'
+export { RDC_CIVIL_EVENTS } from './events'
+export { RDC_CERTIFICATE_CONFIG, buildVerificationUrl } from './certificate/config'
+export { KINSHASA_PILOT, KINSHASA_COMMUNES } from './locations/kinshasa'
+export {
+  KINSHASA_PILOT_OFFICES,
+  OFFICE_DATA_NOTICE
+} from './locations/civil-registration-offices'
